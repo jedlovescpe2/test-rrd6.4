ef7cff120b9ba3cf9b190d2651969b7940291cbf
Jed Llenado <jedlovescpe2@gmail.com>
2022-12-05 02:25:52 +0300

    Initial commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  RouterProvider,
+  BrowserRouter,
+  Route,
+  Routes,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-import BlogLayout from './pages/BlogLayout';
-import BlogPostsPage from './pages/BlogPosts';
-import NewPostPage from './pages/NewPost';
-import PostDetailPage from './pages/PostDetail';
-import RootLayout from './components/RootLayout';
-import WelcomePage from './pages/Welcome';
+import BlogLayout from "./pages/BlogLayout";
+import BlogPostsPage, {
+  loader as blogPostsLoader,
+} from "./pages/BlogPosts";
+import ErrorPage from "./pages/ErrorPage";
+import NewPostPage from "./pages/NewPost";
+import PostDetailPage, {
+  loader as blogPostLoader,
+} from "./pages/PostDetail";
+import RootLayout from "./pages/RootLayout";
+import WelcomePage from "./pages/Welcome";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} errorElement={<ErrorPage/>}>
+      <Route index element={<WelcomePage />} />
+      <Route path="/blog" element={<BlogLayout />}>
+        <Route
+          index
+          element={<BlogPostsPage />}
+          loader={blogPostsLoader}
+        />
+        <Route
+          path=":id"
+          element={<PostDetailPage />}
+          loader={blogPostLoader}
+          // errorElement={<p>Error occured.</p>}
+        />
+      </Route>
+      <Route path="/blog/new" element={<NewPostPage />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index element={<BlogPostsPage />} />
-            <Route path=":id" element={<PostDetailPage />} />
-          </Route>
-          <Route path="/blog/new" element={<NewPostPage />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
+const ErrorPage = () => {
+    const error = useRouteError();
+    return (
+        <>
+        <MainNavigation/>
+        <main id="error-content">
+            <h1>An error occured!</h1>
+            <p>{error.message}</p>
+        </main></>
+    );
+};
+
+export default ErrorPage; 
